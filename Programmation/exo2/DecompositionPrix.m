3b308bd02dc3cd7abae7569f6a88bc6f778ecf69
victorius35 <victor.bertret@gmail.com>
2021-03-09 09:40:03 +0100

    exo2

--- Programmation/exo2/DecompositionPrix.m
+++ Programmation/exo2/DecompositionPrix.m
@@ -1,7 +1,7 @@
 function [u,v,p,k,J] = DecompositionPrix(N,A,C,rho,eps,kmax)
     %N : taille de l'instance du problème
     %A : matrice de la fonction objective de taille (N+1,2,2)
-    %C : matrice de contraintes associée aux sous-problèmes de taille (N+1,2)
+    %C : matrice de contraintes associée aux sous-problèmes de taille (N,2)
     %rho : pas de gradient pour la coordination
     %eps : précision de la décomposition par les prix
     %kmax : nombre d'itérations max
@@ -40,7 +40,7 @@ function [u,v,p,k,J] = DecompositionPrix(N,A,C,rho,eps,kmax)
             b_sp(1:2,1)=p;
             
             C_in=[1 0 0 0 ; -1 0 0 0 ; 0 1 0 0 ; 0 -1 0 0 ; -1 0 -1 0];
-            d_in=[C(i,1) ; 0 ; C(i,2)-C(i,1) ; 0 ; C(i,1)];
+            d_in=[C(i,1) ; 0 ; C(i,2)-C(i,1) ; 0 ; -C(i,1)];
             
             C_eq=ones(1,4);
             d_eq=C(i,2);
@@ -52,13 +52,21 @@ function [u,v,p,k,J] = DecompositionPrix(N,A,C,rho,eps,kmax)
             u(i,:)=temp(1:2);
             v(i,:)=temp(3:4);
             
-            %Incrementation de la valeur objective J
+            %Incrementation de la valeur objective J pour les N premiers
+            %sous-problèmes
             J=J+temp'*A_sp*temp;
             
         end
         
         %Décomposition du N+1ème sous-problème
-        u(N+1,:)=p/C(N+1,:);
+        u(N+1,1)=p(1,1)/A(N+1,1,1); 
+        u(N+1,2)=p(1,2)/A(N+1,2,2);
+        
+        %Incrémentation de la valeur objective pour le N+1ème
+        %sous-problèmes
+        A_sp=zeros(2,2);
+        A_sp(1,1)=A(N+1,1,1) ; A_sp(2,2)=A(N+1,2,2);
+        J=J+(1/2)*(u(N+1,:)*A_sp*u(N+1,:)');
         
         %Coordination:
         p = p + rho.*(sum(u(1:N,:),1)-u(N+1,:));
--- Programmation/exo2/Main.m
+++ Programmation/exo2/Main.m
@@ -10,12 +10,11 @@ A1=zeros(N1+1,2,2);
 A1(1,:,:)=[4 0 ; 0 5];
 A1(2,:,:)=[1 0 ; 0 2];
 
-C1=zeros(N1+1,2);
+C1=zeros(N1,2);
 C1(1,:)=[1 2];
-C1(2,:)=[1 3];
 
 rho = 0.1;
-eps = 10^(-6);
-kmax = 4000;
+eps = 10^(-3);
+kmax = 200;
 
 [u1_prix,v1_prix,p1_prix,k1_prix,J1_prix] = DecompositionPrix(N1,A1,C1,rho,eps,kmax);
